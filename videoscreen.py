6d7efe0dc9f30d105d2c912e762160a37611007b
timelinc0ln <patrickcasey2014@gmail.com>
2014-04-22 20:27:33 -0500

    Found video stream example to work from. Need to work on data flow.

--- videoscreen.py
+++ videoscreen.py
@@ -1,10 +1,13 @@
-import PySide
+# GUI Window for streaming video from aircraft
 import sys
 
 from PySide import QtCore, QtGui
-from PySide.QtCore import QX11EmbedWindow
-from PySide.QtGui import QX11EmbedContainer 
+#from PySide.QtCore import QX11EmbedWindow
+#from PySide.QtGui import QX11EmbedContainer 
 
+## VideoStream class
+# Wrote this one to test window
+#
 class VideoStream(QtGui, QWidget):
 	def __init__(self, parent=None):
 		super(VideoStream, self).__init__(parent)
@@ -18,12 +21,53 @@ class VideoStream(QtGui, QWidget):
 		self.videoLabel.setBackgroundRole(QtGui.QPallette.Dark)
 		self.videoLabel.setAutoFillBackGround(True)
 
+## Example video display class that I found online
+# Does not use QX11EmbedWidget or QX11EmbedContainer
+#
+class CameraDisplay(QtGui.QLabel):
+	def __init__(self):
+		super(CameraDisplay, self).__init__()
+
+	def updateFrame(self, image):
+		self.setPixmap(QtGui.QPixmap.fromImage(image))
+
+
+## Class to define frame layout
+#
+class ControlCenter(QtGui.QWidget):
+	up_camera_signal = QtCore.Signal(QtGui.QImage)
+	up_camera = None
+
+	def __init__(self):
+		super(ControlCenter, self).__init__()
+		self.up_camera = CameraDisplay()
+		self.up_camera_signal.connect(self.up_camera.updateFrame)
+
+		grid = QtGui.QGridLayout()
+		grid.setSpacing(10)
+
+		grid.addWidget(self.up_camera, 0, 0)
+
+		self.setLayout(grid)
+
+		self.setGeometry(300, 300, 350, 300)
+		self.setWindowTitle('Control Center')
+		self.show()
+
+	def up_camera_callback(self, data):
+		'''This function gets called by an external thread'''
+		try:
+		  image = QtGui.QImage(data.data, data.width, data.height, QtGui.QImage.Format_RGB888)
+		  self.up_camera_signal.emit(image)
+
+		except Exception, e:
+		  print(e)
+
+## TODO: Setup Server that will send video information to this appliation 
 
 if __name__ == '__main__':
-
-	import sys
-
-	app = QGui.QApplication(sys.argv)
-	player = MoviePlayer()
-	player.show()
+	app = QtGui.QApplication(sys.argv)
+	ex = ControlCenter()
+	# player = MoviePlayer()
+	# player.show()
 	sys.exit(app.exec_())
