48be96b3bcb4ec90b9378e4df2c29eff5611a433
timelinc0ln <patrickcasey2014@gmail.com>
2014-04-24 22:55:49 -0500

    trying to display image

--- videoscreen.py
+++ videoscreen.py
@@ -1,62 +1,31 @@
 # GUI Window for streaming video from aircraft
 import sys
+import msvcrt as m
 
-from PySide import QtCore, QtGui
-#from PySide.QtCore import QX11EmbedWindow
-#from PySide.QtGui import QX11EmbedContainer 
-
-
-class CameraDisplay(QtGui.QLabel):
-	def __init__(self):
-		super(CameraDisplay, self).__init__()
-
-	def updateFrame(self, image):
-		self.setPixmap(QtGui.QPixmap.fromImage(image))
+from PySide import QtCore, QtGui 
 
 filecount = 0
 ## Class to define frame layout
 #
 class ControlCenter(QtGui.QWidget):
-	up_camera_signal = QtCore.Signal(QtGui.QImage)
-	up_camera = None
-
 	def __init__(self):
 		super(ControlCenter, self).__init__()
-		self.up_camera = CameraDisplay()
-		self.up_camera_signal.connect(self.up_camera.updateFrame)
-
 		self.createButtonsLayout()
-		
 		grid = QtGui.QVBoxLayout()
-		#grid.setSpacing(10)
-
-		grid.addWidget(self.up_camera, 0, 0)
 		grid.addLayout(self.buttonsLayout)		
-
 		self.setLayout(grid)
 
-		self.setGeometry(300, 50, 1280, 920)
+		self.setGeometry(300, 300, 200, 200) # 1920, 700, 200, 200
 		self.setWindowTitle('Video Stream')
 		self.show()
 
-	## Class for getting data to display
-	# TODO: Figure out how to do this...
-	#
-	def up_camera_callback(self, data):
-		'''This function gets called by an external thread'''
-		try:
-		  image = QtGui.QImage(data.data, data.width, data.height, QtGui.QImage.Format_RGB888)
-		  self.up_camera_signal.emit(image)
-
-		except Exception, e:
-		  print(e)
-
 ## Screenshot capabilities
-	def screenCaptureWidget(widget): 
-		filename = "Screenshot"
+
+	def screenCaptureWidget(self): 
+		filename = "/Screenshot"
 		global filecount
 		fileformat = "png"
-		pixmap =  QtGui.QPixmap.grabWidget(widget)
+		pixmap =  QtGui.QPixmap.grabWindow(QtGui.QApplication.desktop().winId(), 500, 500, 1000, 1000) # 1920, 0, 960, 540
 		initialPath = QtCore.QDir.currentPath() + filename + str(filecount) + "." + fileformat 
 		filename = initialPath
 		pixmap.save(filename, fileformat)
@@ -65,7 +34,6 @@ class ControlCenter(QtGui.QWidget):
 	def createButtonsLayout(self):
 		self.saveScreenshotButton = self.createButton("Save Screenshot", self.screenCaptureWidget)
 		self.buttonsLayout = QtGui.QHBoxLayout()
-		#self.addStretch()
 		self.buttonsLayout.addWidget(self.saveScreenshotButton)
 
 	def createButton(self, text, member):
@@ -73,14 +41,35 @@ class ControlCenter(QtGui.QWidget):
 		button.clicked.connect(member)
 		return button
 
-	def updateScreenshotLabel(self):
-		self.screenshotLabel.setPixmap(self.originalPixmap.scaled(
-			self.screenshotLabel.size(), QtCore.Qt.KeepAspectRatio,
-			QtCore.Qt.SmoothTransformation))
+class ScreenShot(QtGui.QWidget):
+	def __init__(self):
+		super(ScreenShot, self).__init__()
+		self.initWidget(filename)
+
+	def initWidget(self):
+		hbox = QtGui.QHBoxLayout(self)
+		pixmap = QtGui.QPixmap(filename)
+
+		lbl = QtGui.QLabel(self)
+		lbl.setPixmap(pixmap)
+
+		hbox.addWidget(lbl)
+		self.setLayout(hbox)
+
+		self.setGeometry(300, 300, 1000, 600)
+		self.setWindowTitle('Image viewer')
+		self.show() 
+		
+
+
+	def pixelSelect( self, event ):
+		print 'hello'
+		position = QPoint( event.pos().x(),  event.pos().y())
+		self.setWindowTitle( 'Pixel position = (' + str( event.pos().x() ) + ' , ' + str( event.pos().y() ))
+
+
 
 if __name__ == '__main__':
 	app = QtGui.QApplication(sys.argv)
 	ex = ControlCenter()
-	# player = MoviePlayer()
-	# player.show()
 	sys.exit(app.exec_())
